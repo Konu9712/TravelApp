175113bccc11eaa85ba5b7fae03256f9a0b242a9
Konu <51238256+Konu9712@users.noreply.github.com>
2023-10-12 15:57:07 -0400

    login API

--- component/auth.js
+++ component/auth.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const User = require("../database/sechma/authSechma");
@@ -9,7 +10,7 @@ router.get("/test", (req, res) => {
 });
 
 router.get("/signup", async (req, res) => {
-  const { email, password } = req.body;
+  let { email, password } = req.body;
 
   try {
     // Check if the user already exists
@@ -18,7 +19,7 @@ router.get("/signup", async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
     var token = jwt.sign({ foo: "bar" }, "travelapp", { expiresIn: "1h" });
-
+    password = await bcrypt.hash(password, 10);
     // Create a new user
     const newUser = new User({ email, password, token });
 
@@ -27,6 +28,30 @@ router.get("/signup", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error while signing up" });
+  }
+});
+
+// Login route
+router.get("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, "travelapp", {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error during login" });
   }
 });
 
