610d14bf3b9e70c4ac7d8ad31a13ac834b375599
Konu <51238256+Konu9712@users.noreply.github.com>
2023-10-29 19:12:19 -0400

    insert location
    Created schema for Location, and developed an API for inserting location

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
-const authRoutes = require("../component/auth");
+const authRoutes = require("../component/Auth/auth");
+const dashboardRoutes = require("../component/Dashboard/dasboard");
 
 module.exports = (app) => {
   app.use("/auth", authRoutes);
+  app.use("/dashboard", dashboardRoutes);
 };
