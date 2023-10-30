80d3651b7d08c814a40b61006451ac3edbc24568
Konu <51238256+Konu9712@users.noreply.github.com>
2023-10-30 15:50:44 -0400

    update in insert locationAPI
    Added some validation for empty string or null string

---
+++ helper/validaion.js
@@ -0,0 +1,16 @@
+class Helper {
+  isEmpty(value) {
+    if (
+      value === undefined ||
+      value === null ||
+      (typeof value === "object" && Object.keys(value).length === 0) ||
+      (typeof value === "string" && value.trim().length === 0)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+const helper = new Helper();
+module.exports = helper;
