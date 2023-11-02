477793d6faa833e2f0af5ce3c0f7a4940eafbc79
Konu <51238256+Konu9712@users.noreply.github.com>
2023-11-01 21:00:15 -0400

    search  screen API
    Gives random images for location schema into different objects with name

--- component/SearchModule/search.js
+++ component/SearchModule/search.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const helper = require("../../helper/validaion");
+const locationSechma = require("../../database/sechma/locationSechma");
 
 const router = express.Router();
 
@@ -9,5 +10,30 @@ router.get("/test", (req, res) => {
 });
 
 // Search Location
+router.post("/allImagesShuffled", async (req, res) => {
+  try {
+    const locations = await locationSechma.find({}, { name: 1, img: 1 });
+    const allImages = [];
+    locations.forEach((location) => {
+      location.img.forEach((image) => {
+        allImages.push({ name: location.name, img: image });
+      });
+    });
+    const shuffledImages = shuffleArray(allImages);
+    res.json(shuffledImages);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Error fetching and shuffling all images" });
+  }
+});
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
 module.exports = router;
