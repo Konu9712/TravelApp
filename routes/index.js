5e851a9c4d2df188d52a0360346bab2c7d50a7e0
Konu <51238256+Konu9712@users.noreply.github.com>
2023-11-16 00:36:15 -0500

    Created an API for a specific location
    It includes activities according to the province, and restaurants is depend on the city

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const authRoutes = require("../component/Auth/auth");
 const dashboardRoutes = require("../component/Dashboard/dasboard");
 const searchRoutes = require("../component/SearchModule/search");
+const locationRoutes = require("../component/Location/location");
 
 module.exports = (app) => {
   app.use("/auth", authRoutes);
   app.use("/dashboard", dashboardRoutes);
   app.use("/searchScreen", searchRoutes);
+  app.use("/location", locationRoutes);
 };
